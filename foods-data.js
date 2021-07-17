a27a50796a5b2b2cafa873a7225fdf6c348d8fa6
mucahitkaya <mucahit.kayaeee@hotmail.com>
2021-07-17 18:03:11 +0300

    explanation in below

--- foods-data.js
+++ foods-data.js
@@ -1,42 +1,43 @@
-import axios from "axios";
 import Fuse from "fuse.js";
 
-function listMakerFunction() {
+//
+
+//bütün ekrana yazdırma işlemini inputtan gelen değeri mainfunc. atarak başlatıyoruz
+function mainFunction(userInput) {
+  //fetch ile apidan gelen datayı jsona parse ederek datafilter funct göndriyoruz
   fetch("https://jsonplaceholder.typicode.com/todos")
     .then((apiRespond) => apiRespond.json())
-    .then((foodData) => listElementMaker(foodData));
+    .then((foodData) => dataFilter(foodData, userInput));
 }
-const listElementMaker = (foodData) => {
-  for (let i = 0; i < 40; i++) {
-    const newList = document.getElementById("main-list");
-    var newFood = document.createElement("li");
-    newFood.textContent = foodData[i].title;
-    newList.appendChild(newFood);
-  }
+//jsona parse edilmiş datayı ve input değerini alarak fuse.js ile api datasında arama yapıyoruz
+const dataFilter = (foodData, userInput) => {
+  const fuse = new Fuse(foodData, {
+    keys: ["id", "title"],
+  });
+  //arama sonucunu searchResults değişkenine atayoruz
+  const searchResults = fuse.search(userInput);
+
+  //listMaker funct. eşleşen sonuçları gönderiyoruz
+  listMaker(searchResults);
 };
 
-function myfunction() {
-  const inputValue = document.getElementById("search-box-input").value;
-  console.log(inputValue);
-  event.preventDefault();
-}
+//eşleşmesi olan elemanları list olarak yazdırmak için
+const listMaker = (foodThatSearched) => {
+  const lengthOfSearchedData = foodThatSearched.length;
+  const searcedDataStorage = [];
 
-export { myfunction, listMakerFunction };
-
-//   const response = await axios(
-//     "https://jsonplaceholder.typicode.com/todos"
-//   ).json();
-//   console.log(response);
-
-//   const fuse = new Fuse(foodData, {
-//     keys: ["title"],
-//   });
-
-// var parsedData = [];
-// async function getTheData() {
-//   const data = await (
-//     await fetch("https://jsonplaceholder.typicode.com/todos")
-//   ).json();
-//   console.log(data);
-//   return data;
-// }
+  foodThatSearched.forEach((element) => {
+    searcedDataStorage.push(element);
+    console.log(searcedDataStorage);
+  });
+  for (var i = 0; i < 12; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "new-div");
+    const newH = document.createElement("h5");
+    const selectedDiv = document.getElementById("searched-food-DOM");
+    selectedDiv.append(newDiv);
+    newH.innerText = searcedDataStorage[i].item.title;
+    newDiv.appendChild(newH);
+  }
+};
+export default mainFunction;
